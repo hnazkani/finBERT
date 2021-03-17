3a7481fe3b0bc0a50efa2af45833b2afdbed551e
ValueQuant <valuequant@gmail.com>
2021-03-17 06:52:13 +0530

    Modify to make the repo work on windows. Corrected main.py to use correct transformers

--- main.py
+++ main.py
@@ -7,18 +7,22 @@ from flask import request
 import sys
 from finbert.finbert import predict
 from pytorch_pretrained_bert.modeling import BertForSequenceClassification
+from transformers import AutoModelForSequenceClassification
 import nltk
 
 nltk.download('punkt')
 app = Flask(__name__)
 CORS(app)
 start = int(round(time.time()))
-model = BertForSequenceClassification.from_pretrained('/src/models/classifier_model/finbert-sentiment', num_labels=3, cache_dir=None)
+
+model = AutoModelForSequenceClassification.from_pretrained('models/classifier_model/finbert-sentiment', num_labels=3, cache_dir=None)
 
 @app.route("/",methods=['POST'])
 def score():
     text=request.get_json()['text']
-    return(predict(text, model).to_json(orient='records'))
+    result_df = predict(text, model)
+    print (result_df)
+    return(result_df.to_json(orient='records'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=False, threaded=True)
