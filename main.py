b9b3bef2c436f207db0bd8d82fb5f74f275e5793
ValueQuant <valuequant@gmail.com>
2021-03-17 17:21:32 +0530

    added the test case in the main body

--- main.py
+++ main.py
@@ -26,3 +26,9 @@ def score():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=False, threaded=True)
+    
+    '''
+    POST request:
+        url: http://0.0.0.0:8080/
+        body: {"text": "Later that day Apple said it was revising down its earnings expectations in the fourth quarter of 2018, largely because of lower sales and signs of economic weakness in China. The news rapidly infected financial markets. Apple’s share price fell by around 7% in after-hours trading and the decline was extended to more than 10% when the market opened. The dollar fell by 3.7% against the yen in a matter of minutes after the announcement, before rapidly recovering some ground. Asian stockmarkets closed down on January 3rd and European ones opened lower. Yields on government bonds fell as investors fled to the traditional haven in a market storm."}
+    '''
